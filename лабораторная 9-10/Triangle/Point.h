a54b74ae4a079dcbee83af2c0ad5080d58986206
Andrey582 <gandrey0606@mail.ru>
2020-10-22 22:58:53 +0300

    Треугольник

--- лабораторная 9-10/Triangle/Point.h
+++ лабораторная 9-10/Triangle/Point.h
@@ -1,14 +1,12 @@
 #ifndef POINT_H
 #define POINT_H
-
 class Point {
 public:
-	Point(double _x = 0, double _y = 0) {
-
-	}
-
+	// Конструктор
+	Point(double _x = 0, double _y = 0) : x(_x), y(_y) {}
+	// Другие методы
 	void Show() const;
+public:
 	double x, y;
 };
-
-#endif // !POINT_H
+#endif /* POINT_H */
